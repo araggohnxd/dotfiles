8602b1f8b8ef6cd345fbf136ba0a7c0b55da75e4
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-13 20:33:32 -0300

    update installation script

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
 
-YELLOW="\033[33m"
-GREEN="\033[32m"
-RESET="\033[0m"
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
+function now_installing() {
+	printf "${YELLOW}Now installing ${GREEN}$@${YELLOW}...${RESET}\n"
+}
 
 # configure dotfiles bare repo
-function config {
+function config() {
 	/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
 }
 
 # this script only works via apt or pacman, sorry
 if command -v pacman >/dev/null; then
 	pm="pacman -S"
-	printf "$YELLOW Your package manager is$GREEN pacman$YELLOW!$RESET\n"
+	printf "${YELLOW}Your package manager is ${GREEN}pacman${YELLOW}!${RESET}\n"
 elif command -v apt >/dev/null; then
 	pm="apt install"
-	printf "$YELLOW Your package manager is$GREEN apt$YELLOW!$RESET\n"
+	printf "${YELLOW}Your package manager is ${GREEN}apt${YELLOW}!${RESET}\n"
 else
-	printf "$YELLOW get a real OS ffs $RESET" && exit 1
+	printf "${YELLOW}get a real OS ffs${RESET}\n" && exit 1
 fi
 
 # checks if git is installed
 if ! command -v git >/dev/null; then
-	printf "$YELLOW Now installing$GREEN git$YELLOW...$RESET\n"
+	now_installing git
 	yes | sudo $pm git
 fi
 
 # checks if zsh is installed
 if ! command -v zsh >/dev/null; then
-	printf "$YELLOW Now installing$GREEN zsh$YELLOW...$RESET\n"
+	now_installing zsh
 	yes | sudo $pm zsh
 fi
 
 # checks if gcc is installed
 if ! command -v gcc >/dev/null; then
-	printf "$YELLOW Now installing$GREEN gcc$YELLOW...$RESET\n"
+	now_installing gcc
 	yes | sudo $pm gcc
 fi
 
@@ -42,16 +46,17 @@ fi
 git clone --bare git@github.com:araggohnxd/dotfiles.git $HOME/dotfiles/
 config checkout
 if [ $? != 0 ]; then # checkout may fail if there are pre-existing dotfiles
-	printf "$YELLOW Backing up pre-existing dotfiles... $RESET\n"
+	printf "${YELLOW}Backing up pre-existing dotfiles...${RESET}\n"
 	mkdir -p .config-backup
 	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi
 config checkout
-printf "$YELLOW Checked out config!$RESET\n"
+printf "${YELLOW}Checked out config!${RESET}\n"
 config config status.showUntrackedFiles no
 
 # checks if rust/cargo is installed
 if ! command -v cargo >/dev/null; then
+	now_installing cargo
 	curl https://sh.rustup.rs -sSf | sh
 	config reset --hard
 fi
@@ -60,4 +65,4 @@ source "$HOME/.cargo/env"
 # install rust utilitaries
 cargo install cargo-update zoxide exa bat procs ytop
 
-printf "\n\n$YELLOW Now run$GREEN exec zsh$YELLOW to finish the setup!$RESET\n\n"
+printf "\n\n${YELLOW}Now run '${GREEN}exec zsh${YELLOW}' to finish the setup!${RESET}\n\n"
