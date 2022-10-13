283a98f24ace2efc4759f834fa8b84266858906c
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-13 06:09:06 -0300

    colors are cooler

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+YELLOW="\033[33m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
 # configure dotfiles bare repo
 function config {
 	/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
@@ -8,37 +12,42 @@ function config {
 # this script only works via apt or pacman, sorry
 if command -v pacman >/dev/null; then
 	pm="pacman -S"
+	printf "$YELLOW Your package manager is$GREEN pacman$YELLOW!$RESET\n"
 elif command -v apt >/dev/null; then
 	pm="apt install"
+	printf "$YELLOW Your package manager is$GREEN apt$YELLOW!$RESET\n"
 else
-	echo get a real OS ffs && exit 1
+	printf "$YELLOW get a real OS ffs $RESET" && exit 1
 fi
 
 # checks if git is installed
 if ! command -v git >/dev/null; then
 	yes | sudo $pm git
+	printf "$YELLOW Now installing$GREEN git$YELLOW...$RESET\n"
 fi
 
 # checks if zsh is installed
 if ! command -v zsh >/dev/null; then
 	yes | sudo $pm zsh
+	printf "$YELLOW Now installing$GREEN zsh$YELLOW...$RESET\n"
 fi
 
 # checks if gcc is installed
 if ! command -v gcc >/dev/null; then
 	yes | sudo $pm gcc
+	printf "$YELLOW Now installing$GREEN gcc$YELLOW...$RESET\n"
 fi
 
 # clone dotfiles bare repo
 git clone --bare git@github.com:araggohnxd/dotfiles.git $HOME/dotfiles/
 config checkout
 if [ $? != 0 ]; then # checkout may fail if there are pre-existing dotfiles
-	echo "Backing up pre-existing dotfiles"
+	printf "$YELLOW Backing up pre-existing dotfiles... $RESET\n"
 	mkdir -p .config-backup
 	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi
 config checkout
-echo "Checked out config"
+printf "$YELLOW Checked out config!$RESET\n"
 config config status.showUntrackedFiles no
 
 # checks if rust/cargo is installed
@@ -51,4 +60,4 @@ source "$HOME/.cargo/env"
 # install rust utilitaries
 cargo install cargo-update zoxide exa bat procs ytop
 
-printf '\n\n\033[33mNow run `\033[32mexec zsh\033[33m` to finish the setup!\033[0m\n\n'
+printf "\n\n$YELLOW Now run$GREEN exec zsh$YELLOW to finish the setup!$RESET\n\n"
